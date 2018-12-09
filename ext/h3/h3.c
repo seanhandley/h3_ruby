d7a8ff3f3f9b565f59e826a623298853cba5e5ed
Sean Handley <sean.handley@gmail.com>
2018-12-09 09:17:06 +0000

    Adding geo_to_h3.
    Borrowed some code from StuartGeo for this.

--- ext/h3/h3.c
+++ ext/h3/h3.c
@@ -1,13 +1,17 @@
 #include <ruby.h>
 #include <h3/h3api.h>
 
-static VALUE hello_world(VALUE mod)
-{
-  return rb_str_new2("hello world");
-}
+#define TO_RADIANS(degrees) (degrees)*M_PI/180.0
+
+static GeoCoord to_geocoord(VALUE coords);
 
 static VALUE h3_maxKringSize(VALUE mod, VALUE k) {
   return INT2NUM(maxKringSize(NUM2INT(k)));
+}
+
+static VALUE h3_geoToH3(VALUE mod, VALUE coords, VALUE res) {
+  GeoCoord h3_coords = to_geocoord(coords);
+  return LONG2NUM(geoToH3(&h3_coords, NUM2INT(res)));
 }
 
 /* --- Initialization -------------------------------------------------------------------------- */
@@ -16,6 +20,28 @@ static VALUE h3_maxKringSize(VALUE mod, VALUE k) {
 void Init_h3()
 {
     VALUE h3_ruby = rb_define_module("H3Ruby");
-    rb_define_singleton_method(h3_ruby, "hello_world", hello_world, 0);
     rb_define_singleton_method(h3_ruby, "max_kring_size", h3_maxKringSize, 1);
+    rb_define_singleton_method(h3_ruby, "geo_to_h3", h3_geoToH3, 2);
+}
+
+static GeoCoord to_geocoord(VALUE coords)
+{
+    if (TYPE(coords) != T_ARRAY) {
+        ID method = rb_intern("to_coordinates");
+        if (rb_respond_to(coords, method)) {
+            coords = rb_funcall(coords, method, 0);
+            Check_Type(coords, T_ARRAY);
+        } else {
+            rb_raise(rb_eTypeError, "%+"PRIsVALUE" are not valid coordinates", coords);
+        }
+    }
+
+    if (RARRAY_LEN(coords) != 2) {
+        rb_raise(rb_eArgError, "%+"PRIsVALUE" should have exactly two coordinates", coords);
+    }
+
+    return (GeoCoord) {
+        TO_RADIANS(NUM2DBL(rb_ary_entry(coords, 0))),
+        TO_RADIANS(NUM2DBL(rb_ary_entry(coords, 1)))
+    };
 }
--- spec/h3_ruby_spec.rb
+++ spec/h3_ruby_spec.rb
@@ -1,23 +1,50 @@
 require "bigdecimal"
 
 RSpec.describe H3Ruby do
-  describe ".hello_world" do
-    subject(:hello_world) { H3Ruby.hello_world }
-
-    let(:result) { "hello world" }
-
-    it "returns the expected result" do
-      expect(hello_world).to eq(result)
-    end
-  end
-
   describe ".max_kring_size" do
-    subject(:max_kring_size) { H3Ruby.max_kring_size(2) }
+    subject(:max_kring_size) { H3Ruby.max_kring_size(k) }
 
+    let(:k) { 2 }
     let(:result) { 19 }
 
     it "returns the expected result" do
       expect(max_kring_size).to eq(result)
+    end
+
+    context "when provided with a bad k value" do
+      let(:k) { "boom" }
+
+      it "raises an error" do
+        expect { max_kring_size }.to raise_error(TypeError)
+      end
+    end
+  end
+
+  describe ".geo_to_h3" do
+    let(:resolution) { 8 }
+    let(:coords) { [53.959130, -1.079230]}
+    subject(:geo_to_h3) { H3Ruby.geo_to_h3(coords, resolution) }
+
+    let(:result) { "8819429a9dfffff".to_i(16) }
+
+    it "returns the expected result" do
+      expect(geo_to_h3).to eq(result)
+    end
+
+    context "when given more than 2 values" do
+      let(:coords) { [1, 2, 3] }
+
+      it "raises an error" do
+        expect { geo_to_h3 }.to raise_error(ArgumentError)
+      end
+    end
+
+    context "when given a non array" do
+      let(:coords) { "boom" }
+
+      it "raises an error" do
+        expect { geo_to_h3 }.to raise_error(TypeError)
+      end
     end
   end
 end
