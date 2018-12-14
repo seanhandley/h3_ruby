2f56cf9ecb0552974840d6635b5f04663dfe0954
Sean Handley <sean.handley@gmail.com>
2018-12-14 11:39:37 +0000

    Add all structs from h3api.h

--- lib/h3/structs.rb
+++ lib/h3/structs.rb
@@ -11,7 +11,45 @@ module H3
 
     class GeoBoundary < FFI::Struct
       layout :num_verts, :int,
-             :verts, [:double, 20]
+             :verts, [:double, 20] # array of GeoCoord structs (must be fixed length)
+    end
+
+    class GeoFence < FFI::Struct
+      layout :num_verts, :int,
+             :verts, :pointer
+    end
+
+    class GeoPolygon < FFI::Struct
+      layout :geofence, GeoFence,
+             :num_holes, :int,
+             :holes, :pointer # array of GeoFence structs
+    end
+
+    class GeoMultiPolygon < FFI::Struct
+      layout :num_polygons, :int,
+             :polygons, :pointer # array of GeoPolygon structs
+    end
+
+    class LinkedGeoCoord < FFI::Struct
+      layout :vertex, GeoCoord,
+             :next, LinkedGeoCoord.ptr
+    end
+
+    class LinkedGeoLoop < FFI::Struct
+      layout :first, LinkedGeoCoord.ptr,
+             :last, LinkedGeoCoord.ptr,
+             :next, LinkedGeoLoop.ptr
+    end
+
+    class LinkedGeoPolygon < FFI::Struct
+      layout :first, LinkedGeoLoop.ptr,
+             :last, LinkedGeoLoop.ptr,
+             :next, LinkedGeoPolygon.ptr
+    end
+
+    class CoordIJ < FFI::Struct
+      layout :i, :int,
+             :j, :int
     end
   end
 end
