77295c5b82860bd1bed5d98a8d362d983444386b
Sean Handley <sean.handley@gmail.com>
2018-12-17 09:35:54 +0000

    Polygon functions.

--- spec/h3_spec.rb
+++ spec/h3_spec.rb
@@ -846,4 +846,164 @@ RSpec.describe H3 do
     end
   end
 
+  # http://geojson.io is helpful for setting up test fixtures
+  describe ".geo_json_to_coordinates" do
+    let(:input) do
+      File.read(File.join(File.dirname(__FILE__), "support/fixtures/banbury.json"))
+    end
+    let(:coordinates) do
+      [
+        [
+          [52.24630137198303, -1.7358398437499998],
+          [52.05249047600099, -1.8923950195312498],
+          [51.891749018068246, -1.56829833984375],
+          [51.91208502557545, -1.27716064453125],
+          [52.032218104145294, -1.19476318359375],
+          [52.19413974159753, -1.24420166015625],
+          [52.24125614966341, -1.5902709960937498],
+          [52.24630137198303, -1.7358398437499998]
+        ],
+        [
+          [52.12590076522272, -1.58203125],
+          [52.12590076522272, -1.476287841796875],
+          [52.075285904832334, -1.46392822265625],
+          [52.06937709602395, -1.58203125],
+          [52.12590076522272, -1.58203125]
+        ],
+        [
+          [52.01531743663362, -1.4556884765625],
+          [51.97642166216334, -1.483154296875],
+          [51.96626938051444, -1.3677978515625],
+          [52.0102459910103, -1.3568115234375],
+          [52.01531743663362, -1.4556884765625]
+        ]
+      ]
+    end
+
+    subject(:geo_json_to_coordinates) { H3.geo_json_to_coordinates(input) }
+
+    it { is_expected.to eq coordinates }
+
+    context "when given a feature" do
+      let(:input) do
+        File.read(File.join(File.dirname(__FILE__), "support/fixtures/banbury_feature.json"))
+      end
+
+      it { is_expected.to eq coordinates }
+    end
+
+    context "when given a feature collection" do
+      let(:input) do
+        File.read(File.join(File.dirname(__FILE__), "support/fixtures/banbury_feature_collection.json"))
+      end
+
+      it { is_expected.to eq coordinates }
+    end
+  end
+
+  describe ".coordinates_to_geo_json" do
+    let(:input) do
+      [
+        [
+          [52.24630137198303, -1.7358398437499998],
+          [52.05249047600099, -1.8923950195312498],
+          [51.891749018068246, -1.56829833984375],
+          [51.91208502557545, -1.27716064453125],
+          [52.032218104145294, -1.19476318359375],
+          [52.19413974159753, -1.24420166015625],
+          [52.24125614966341, -1.5902709960937498],
+          [52.24630137198303, -1.7358398437499998]
+        ],
+        [
+          [52.12590076522272, -1.58203125],
+          [52.12590076522272, -1.476287841796875],
+          [52.075285904832334, -1.46392822265625],
+          [52.06937709602395, -1.58203125],
+          [52.12590076522272, -1.58203125]
+        ],
+        [
+          [52.01531743663362, -1.4556884765625],
+          [51.97642166216334, -1.483154296875],
+          [51.96626938051444, -1.3677978515625],
+          [52.0102459910103, -1.3568115234375],
+          [52.01531743663362, -1.4556884765625]
+        ]
+      ]
+    end
+    let(:geojson) do
+      File.read(File.join(File.dirname(__FILE__), "support/fixtures/banbury.json"))
+    end
+
+    subject(:coordinates_to_geo_json) { H3.coordinates_to_geo_json(input) }
+
+    it { is_expected.to eq geojson }
+  end
+
+  describe ".max_polyfill_size" do
+    let(:geojson) do
+      File.read(File.join(File.dirname(__FILE__), "support/fixtures/banbury.json"))
+    end
+    let(:resolution) { 9 }
+    let(:expected_count) { 33_391 }
+
+    subject(:max_polyfill_size) { H3.max_polyfill_size(geojson, resolution) }
+
+    it "has the correct number of hexagons" do
+      expect(max_polyfill_size).to eq expected_count
+    end
+  end
+
+  describe ".polyfill" do
+    let(:geojson) do
+      File.read(File.join(File.dirname(__FILE__), "support/fixtures/banbury_without_holes.json"))
+    end
+    let(:resolution) { 9 }
+    let(:expected_count) { 14_369 }
+
+    subject(:polyfill) { H3.polyfill(geojson, resolution) }
+
+    it "has the correct number of hexagons" do
+      expect(polyfill.count).to eq expected_count
+    end
+
+    context "when banbury area has two holes in it" do
+      let(:geojson) do
+        File.read(File.join(File.dirname(__FILE__), "support/fixtures/banbury.json"))
+      end
+      let(:expected_count) { 13_526 }
+
+      it "has fewer hexagons" do
+        expect(polyfill.count).to eq expected_count
+      end
+    end
+
+    context "when polyfilling australia" do
+      let(:geojson) do
+        File.read(File.join(File.dirname(__FILE__), "support/fixtures/australia.json"))
+      end
+      let(:expect_count) { 92 }
+
+      it "has the correct number of hexagons" do
+        expect(polyfill.count).to eq expect_count
+      end
+    end
+  end
+
+  describe ".h3_set_to_linked_geo" do
+    let(:geojson) do
+      File.read(File.join(File.dirname(__FILE__), "support/fixtures/banbury.json"))
+    end
+    let(:resolution) { 8 }
+    let(:hexagons) { H3.polyfill(geojson, resolution) }
+
+    subject(:h3_set_to_linked_geo) { H3.h3_set_to_linked_geo(hexagons) }
+    
+    it "has 3 outlines" do
+      h3_set_to_linked_geo.count == 3
+    end
+
+    it "can be converted to GeoJSON" do
+      expect(H3.coordinates_to_geo_json(h3_set_to_linked_geo)).to be_truthy
+    end
+  end
 end
