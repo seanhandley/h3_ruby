98c31179ffcfeec0a415e3e7836f3846ebf58be4
Sean Handley <sean.handley@gmail.com>
2018-12-20 10:28:32 +0000

    Missing file.

---
+++ lib/h3/bindings.rb
@@ -0,0 +1,12 @@
+require "h3/bindings/base"
+require "h3/bindings/structs"
+require "h3/bindings/private"
+
+module H3
+  # Internal bindings related modules and classes.
+  #
+  # These are intended to be used by the library's public methods
+  # and not to be used directly by client code.
+  module Bindings
+  end
+end
