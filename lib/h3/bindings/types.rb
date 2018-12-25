90eb13ceeeb26c10a6817558917a5d2dc75b2db3
Sean Handley <sean.handley@gmail.com>
2018-12-25 09:12:27 +0000

    Missing docs.

--- lib/h3/bindings/types.rb
+++ lib/h3/bindings/types.rb
@@ -1,11 +1,16 @@
 module H3
   module Bindings
+    # Custom FFI Types
     module Types
+      # A H3 resolution value.
+      #
+      # Integer, but must be between 0 and 15 inclusive.
       class Resolution
         extend FFI::DataConverter
         native_type FFI::Type::INT
 
         RES_RANGE = 0..15
+        private_constant :RES_RANGE
 
         class << self
           def to_native(value, _context)
