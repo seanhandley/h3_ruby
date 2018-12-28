9a7253f14b8730da465b80b809044abcf1ab6ef3
Sean Handley <sean.handley@gmail.com>
2018-12-28 10:27:53 +0000

    Undefine predicate method names without question marks.

--- lib/h3.rb
+++ lib/h3.rb
@@ -26,12 +26,16 @@ module H3
     include Regions
     include Traversal
     include UnidirectionalEdges
-  end
 
-  PREDICATES = %i[h3_indexes_neighbors h3_pentagon h3_res_class_3
-                  h3_unidirectional_edge_valid h3_valid].freeze
-  private_constant :PREDICATES
-  PREDICATES.each do |predicate|
-    singleton_class.send(:alias_method, "#{predicate}?".to_sym, predicate)
+    # FFI's attach_function doesn't allow method names ending with a
+    # question mark. This works around the issue by dynamically
+    # renaming those methods afterwards.
+    PREDICATES = %i[h3_indexes_neighbors h3_pentagon h3_res_class_3
+                    h3_unidirectional_edge_valid h3_valid].freeze
+    private_constant :PREDICATES
+    PREDICATES.each do |predicate|
+      alias_method "#{predicate}?".to_sym, predicate
+      undef_method predicate
+    end
   end
 end
