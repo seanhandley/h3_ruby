94808ad7519c790e256bdfa9a061b81a4ce32023
Sean Handley <sean.handley@gmail.com>
2019-01-17 13:55:56 +0000

    More minor refactor.

--- lib/h3.rb
+++ lib/h3.rb
@@ -26,13 +26,14 @@ module H3
   extend Traversal
   extend UnidirectionalEdges
 
+  PREDICATES = %i[h3_indexes_neighbors h3_pentagon h3_res_class_3
+                  h3_unidirectional_edge_valid h3_valid].freeze
+  private_constant :PREDICATES
+
   class << self
     # FFI's attach_function doesn't allow method names ending with a
     # question mark. This works around the issue by dynamically
     # renaming those methods afterwards.
-    PREDICATES = %i[h3_indexes_neighbors h3_pentagon h3_res_class_3
-                    h3_unidirectional_edge_valid h3_valid].freeze
-    private_constant :PREDICATES
     PREDICATES.each do |predicate|
       alias_method "#{predicate}?", predicate
       undef_method predicate
